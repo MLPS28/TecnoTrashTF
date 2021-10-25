c4e5c507328ee8a8f16731ceaaa73bc983daf33a
MLPS28 <mercedesparedes.silva@gmail.com>
2021-10-25 18:38:44 -0500

    controller y front

---
+++ src/main/java/pe/edu/upc/spring/controller/DireccionController.java
@@ -0,0 +1,98 @@
+package pe.edu.upc.spring.controller;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.sun.el.parser.ParseException;
+
+import pe.edu.upc.spring.model.Reporte;
+import pe.edu.upc.spring.service.IReporteService;
+
+@Controller 	
+@RequestMapping("/direccion") //que atienda el controlador /race  (cuando hay muchos controladores), pero este no muestra nada, tiene q ir a bienvenido
+public class DireccionController {
+	@Autowired
+	private IReporteService uService;
+	
+	@RequestMapping("/bienvenido")
+	public String irPaginaBienvenida() {
+		return "bienvenido"; // "bienvenido" es una pagina del frontEnd, pagina de Inicio
+	}
+	
+	@RequestMapping("/")
+	public String irPaginaListadoUsuarios(Map<String, Object> model) {
+		model.put("listaReportes", uService.listar());
+		return "listReporte"; // "listRace" es una pagina del frontEnd para listar
+	}
+
+	@RequestMapping("/irRegistrar")
+	public String irPaginaRegistrar(Model model) {
+		model.addAttribute("reporte", new Reporte());
+		return "reporte"; // "race" es una pagina del frontEnd para insertar y/o modificar
+	}
+	
+	@RequestMapping("/registrar")
+	public String registrar(@ModelAttribute Reporte objReporte, BindingResult binRes, Model model) 
+		throws ParseException
+	{
+		if (binRes.hasErrors())
+			return "reporte";
+		else {
+			boolean flag = uService.grabar(objReporte);
+			if (flag)
+				return "redirect:/usuario/listar";
+			else {
+				model.addAttribute("mensaje", "Ocurrio un problema");
+				return "redirect:/usuario/irRegistrar";
+			}
+		}
+	}
+	
+	@RequestMapping("/modificar/{id}")
+	public String modificar(@PathVariable int id, Model model, RedirectAttributes objRedir) 
+		throws ParseException
+	{
+		Optional<Reporte> objReporte = uService.listarId(id);
+		if (objReporte == null) {
+			objRedir.addFlashAttribute("mensaje", "Ocurrio un problema");
+			return "redirect:/usuario/listar";
+		}
+		else {
+			model.addAttribute("usuario",objReporte);
+			return "usuario";
+		}
+	}
+		
+	@RequestMapping("/eliminar")
+	public String eliminar(Map<String, Object> model,  @RequestParam(value="id") Integer id) {
+		try {
+			if (id!=null && id>0) {
+				uService.eliminar(id);
+				model.put("listaReportes", uService.listar());
+			}
+		}
+		catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			model.put("mensaje", "Ocurrio un problema");
+			model.put("listaReportes", uService.listar());
+		}
+		return "listReporte";
+	}
+		
+	@RequestMapping("/listar")
+	public String listar(Map<String, Object> model ) {
+		model.put("listaReportes", uService.listar());
+		return "listReporte";
+	}
+	
+}
--- src/main/java/pe/edu/upc/spring/serviceimpl/DistritoServiceImpl.java
+++ src/main/java/pe/edu/upc/spring/serviceimpl/DistritoServiceImpl.java
@@ -7,21 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import pe.edu.upc.spring.model.Distrito;
-import pe.edu.upc.spring.repository.IDistritoRepository;
-import pe.edu.upc.spring.service.IDistritoService;
+import pe.edu.upc.spring.model.Direccion;
+import pe.edu.upc.spring.repository.IDireccionRepository;
+import pe.edu.upc.spring.service.IDireccionService;
 
 @Service
-public class DistritoServiceImpl implements IDistritoService {
+public class DistritoServiceImpl implements IDireccionService {
 
 	@Autowired //ya no es inject
-	private IDistritoRepository dDistrito;  //drace: objeto o representacion de IRaceRepository
+	private IDireccionRepository dDireccion;  //drace: objeto o representacion de IRaceRepository
 	
 	@Override
 	@Transactional //los que no impliquen listados solo seran de tipo transactional
-	public boolean grabar(Distrito distrito) {
-		Distrito objDistrito = dDistrito.save(distrito);
-		if (objDistrito == null)
+	public boolean grabar(Direccion direccion) {
+		Direccion objDireccion = dDireccion.save(direccion);
+		if (objDireccion == null)
 			return false;
 		else
 			return true;
@@ -29,20 +29,20 @@ public class DistritoServiceImpl implements IDistritoService {
 
 	@Override
 	@Transactional
-	public void eliminar(int CDistrito) {
-		dDistrito.deleteById(CDistrito); //solo necesita el id que se desea eliminar
+	public void eliminar(int CDireccion) {
+		dDireccion.deleteById(CDireccion); //solo necesita el id que se desea eliminar
 	}
 
 	@Override
 	@Transactional(readOnly = true)
-	public Optional<Distrito> listarId(int CDistrito) {
-		return dDistrito.findById(CDistrito);
+	public Optional<Direccion> listarId(int CDireccion) {
+		return dDireccion.findById(CDireccion);
 	}
 
 	@Override
 	@Transactional(readOnly = true)
-	public List<Distrito> listar() {
-		return dDistrito.findAll(); //devuelve todo en una lista
+	public List<Direccion> listar() {
+		return dDireccion.findAll(); //devuelve todo en una lista
 	}
 
 }
